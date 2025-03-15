d615fecb9f2c84f013f3f13056bd053aaa4ebba9
Loganrap5 <148388659+Loganrap5@users.noreply.github.com>
2025-03-14 22:58:16 -0500

    -Algorithims Commit

--- algorithms/include/algo.hpp
+++ algorithms/include/algo.hpp
@@ -5,13 +5,16 @@
 
 long fibonacci(const int n)
 { 
-    if (n <= 1) return n;
+    /*if (n <= 1) return n;*/
+    if (n == 0 || n == 1) {
+        return n;
+    }
 
-    int a = 0;
-    int b = 1;
+    long a = 0;
+    long b = 1;
 
     for (int i = 2; i <= n; i++) {
-        int temp = a + b;
+        long temp = a + b;
         a = b;
         b = temp;
     }
@@ -21,13 +24,26 @@ long fibonacci(const int n)
 }
 
 int linear_search(Array * a, const int target)
-{
-    return 0;
+{  
+    for (size_t i = 0; i < a->len; ++i) { 
+        if (a->data[i] == target) {
+            return static_cast<int>(i);  
+        }
+    }
+    return -1;  // Return -1 if not found
+   
 }
 
 long factorial(const int n)
-{
-    return 0;
+{ 
+    if (n == 0)
+    {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+
+
 }
 
 #endif // ALGO_H_
